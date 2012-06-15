bd0a45fbe0ed4724e29143c1d18e85248c71afd9
Leo Trubach <leotrubach@gmail.com>
2012-06-16 05:03:51 +0600

    ticket:1 Modified main page view and templates

--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -1,8 +1,9 @@
 from annoying.decorators import render_to
 from django.contrib.auth.models import User
 
+from .models import Owner
 
 @render_to('hello/home.html')
 def home(request):
-    users = User.objects.filter()
-    return {'users': users}
+    owner = Owner.objects.get(active=True)
+    return {'owner': owner}
