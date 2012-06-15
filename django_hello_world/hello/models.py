098130dd5b40991c156a2e981787005480925114
Leo Trubach <leotrubach@gmail.com>
2012-06-16 04:59:53 +0600

    ticket:1 Added Owner model and added it to admin interface

--- django_hello_world/hello/models.py
+++ django_hello_world/hello/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Owner(models.Model):
+    firstname = models.CharField(max_length=25, verbose_name='first name')
+    lastname = models.CharField(max_length=25, verbose_name='last name')
+    birthday = models.DateField(verbose_name='birthday')
+    bio = models.TextField(verbose_name='bio')
+    email = models.EmailField(verbose_name='email')
+    jabber = models.CharField(max_length=25, verbose_name='jabber')
+    skype = models.CharField(max_length=25, verbose_name='skype')
+    other = models.TextField(verbose_name='other information')
+    active = models.BooleanField(verbose_name='active')
+
+    def __unicode__(self):
+        return '%(name)s %(lastname)s' % {'name': self.firstname,
+                                          'lastname': self.lastname}
+
+    class Meta:
+        verbose_name = 'owner'
+        verbose_name_plural = 'owners'
