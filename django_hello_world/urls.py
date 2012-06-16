2423bfa800bc7ed48b45455751f91f56321bd5d4
Leo Trubach <leotrubach@gmail.com>
2012-06-16 21:00:58 +0600

    ticket:5 Added auth to main page

--- django_hello_world/urls.py
+++ django_hello_world/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
@@ -15,6 +15,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
 )
 
 if settings.DEBUG:
