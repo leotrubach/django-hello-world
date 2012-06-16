6740d445b094624029e99df06e4d97a6a0d9bc35
Leo Trubach <leotrubach@gmail.com>
2012-06-16 14:44:52 +0600

    ticket:3 Added Request model

--- django_hello_world/hello/models.py
+++ django_hello_world/hello/models.py
@@ -18,3 +18,17 @@ class Owner(models.Model):
     class Meta:
         verbose_name = 'owner'
         verbose_name_plural = 'owners'
+
+class Request(models.Model):
+    method = models.CharField(max_length=10, verbose_name='method')
+    path = models.TextField(verbose_name='path')
+    dt_request = models.DateTimeField(auto_now_add=True, verbose_name='request time')
+
+    def __unicode__(self):
+        return '%(dt_request)s %(method)s %(path)s' % {'dt_request': self.dt_request,
+                                                       'path': self.path,
+                                                       'method': self.method}
+
+    class Meta:
+        verbose_name = 'request'
+        verbose_name_plural = 'requests'
