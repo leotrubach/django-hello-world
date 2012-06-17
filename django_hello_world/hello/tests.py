833d1fac20a50d76c3fecd282efbb8a78059d09a
Leo Trubach <leotrubach@gmail.com>
2012-06-17 18:40:26 +0600

    ticket:4 Removed unnecessary import

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -27,7 +27,6 @@ class HttpTest(TestCase):
         self.assertIn('last_requests', response.context)
 
     def test_context_processors(self):
-        from django_hello_world.settings import TEMPLATE_CONTEXT_PROCESSORS
         from django.conf import settings
         c = Client()
         response = c.get(reverse('home'))
