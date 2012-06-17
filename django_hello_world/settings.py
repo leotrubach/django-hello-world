0f2f51c24ae6e8601a9ffb4de23a2a1b377a5a18
Leo Trubach <leotrubach@gmail.com>
2012-06-17 15:41:00 +0600

    ticket:3 updated helper method to include subdirectory parameter, modifications to settings

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -1,8 +1,6 @@
-import os
 from .utils import get_project_path
-# Django settings for django_hello_world project.
 
-PROJECT_PATH = get_project_path()
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -75,7 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'static'),
+    get_project_path('static'),
 )
 
 # List of finder classes that know how to find static files in
