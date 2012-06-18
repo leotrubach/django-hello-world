380d33ee9c4cf998d9960fb558221c2c37b516c0
Leo Trubach <leotrubach@gmail.com>
2012-06-19 02:40:12 +0600

    ticket:13 Added pagination

--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -29,9 +29,14 @@ class RequestList(ListView):
     context_object_name = 'last_requests'
     paginate_by = 20
 
+    def __init__(self, *args, **kwargs):
+        super(ListView, self).__init__(*args, **kwargs)
+        self.priority = 1
+
     def get_context_data(self, **kwargs):
         context = super(RequestList, self).get_context_data(**kwargs)
-        context['form'] = PriorityForm()
+        context['form'] = PriorityForm(initial={'priority': self.priority})
+        context['priority'] = self.priority
         return context
 
     def get_queryset(self):
@@ -39,10 +44,10 @@ class RequestList(ListView):
             # default priority is 1 = descending
             f = PriorityForm(self.request.GET)
             if f.is_valid():
-                priority = f.cleaned_data.get('priority', 1)
+                self.priority = f.cleaned_data.get('priority', 1)
             else:
-                priority = 1
-            if priority == 1:
+                self.priority = 1
+            if self.priority == 1:
                 queryset = Request.objects.order_by('-logged_date')
             else:
                 queryset = Request.objects.order_by('logged_date')
