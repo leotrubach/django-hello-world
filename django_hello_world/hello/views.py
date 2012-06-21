fd1b03555a0431a4907e2bffe188ba2a13dd81d8
Leo Trubach <leotrubach@gmail.com>
2012-06-22 02:18:50 +0600

    ticket:13 Added ordering to request list

--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -25,14 +25,25 @@ class RequestList(ListView):
     context_object_name = 'last_requests'
     paginate_by = 20
 
+    def __init__(self, **kwargs):
+        super(ListView, self).__init__(**kwargs)
+        self.order = 'desc'
+
+    def get_context_data(self, **kwargs):
+        context = super(ListView, self).get_context_data(**kwargs)
+        context['order'] = self.order
+        return context
+
     def get_queryset(self):
         if self.request.method == 'GET':
             # default ordering is descending
             order = self.request.GET.get('order', 'desc')
             if order == 'desc':
-                queryset = Request.objects.order_by('-logged_date')
+                queryset = Request.objects.order_by('-priority', '-logged_date')
+                self.order = 'desc'
             else:
-                queryset = Request.objects.order_by('logged_date')
+                queryset = Request.objects.order_by('priority', '-logged_date')
+                self.order = 'asc'
             return queryset
 
 
