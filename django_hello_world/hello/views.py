4fafb6b8fb7bdf695bef8beeb169122bdf977ea0
Leo Trubach <leotrubach@gmail.com>
2012-06-21 21:01:17 +0600

    Revert "ticket:13 Added view and made modifications to template"
    This reverts commit be269d559fa112907331fce384418ae7688ad184.
    
    Conflicts:
    
    	django_hello_world/hello/forms.py

--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -1,9 +1,9 @@
 from annoying.decorators import render_to
 from django.contrib.auth.models import User
-from django.views.generic import UpdateView, ListView
+from django.views.generic import UpdateView
 
 from .models import Owner, Request
-from .forms import OwnerForm, PriorityForm
+from .forms import OwnerForm
 
 
 @render_to('hello/home.html')
@@ -21,37 +21,6 @@ def home(request):
 def last_ten_requests(request):
     last_requests = Request.objects.order_by('-logged_date')[:10]
     return {'last_requests': last_requests}
-
-
-class RequestList(ListView):
-    model = Request
-    template_name = 'hello/last_requests.html'
-    context_object_name = 'last_requests'
-    paginate_by = 20
-
-    def __init__(self, *args, **kwargs):
-        super(ListView, self).__init__(*args, **kwargs)
-        self.priority = 1
-
-    def get_context_data(self, **kwargs):
-        context = super(RequestList, self).get_context_data(**kwargs)
-        context['form'] = PriorityForm(initial={'priority': self.priority})
-        context['priority'] = self.priority
-        return context
-
-    def get_queryset(self):
-        if self.request.method == 'GET':
-            # default priority is 1 = descending
-            f = PriorityForm(self.request.GET)
-            if f.is_valid():
-                self.priority = f.cleaned_data.get('priority', 1)
-            else:
-                self.priority = 1
-            if self.priority == 1:
-                queryset = Request.objects.order_by('-logged_date')
-            else:
-                queryset = Request.objects.order_by('logged_date')
-            return queryset
 
 
 class EditOwner(UpdateView):
